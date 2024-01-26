ccc2a691bd7df9b95e48c18dd47008180466c5e5
Aleksander Kulej <90187895+aleksanderkulej@users.noreply.github.com>
2024-01-26 06:55:24 +0100

    Add files via upload

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by AK Biznes on 29/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
