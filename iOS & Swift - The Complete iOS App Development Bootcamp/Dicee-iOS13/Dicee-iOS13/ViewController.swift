df2eb241fea94d19cbbce0b1a3ccf9ac53cd8dd1
Aleksander Kulej <90187895+aleksanderkulej@users.noreply.github.com>
2024-01-26 06:49:37 +0100

    Add files via upload

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+   
+
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageViewOne.image = diceArray.randomElement()
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+
+        
+        
+        
+        
+    }
+    
+}
+
