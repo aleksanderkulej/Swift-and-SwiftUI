a52acbd940eeed37751deed87c79fe13e7e18087
Aleksander Kulej <90187895+aleksanderkulej@users.noreply.github.com>
2024-01-26 06:53:42 +0100

    Add files via upload

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by AK Biznes on 20/01/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
